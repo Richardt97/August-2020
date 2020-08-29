5b633a37240e24bb4b784843da23bff51a76a468
Richardt97 <54861755+Richardt97@users.noreply.github.com>
2020-08-28 19:58:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ document.body.innerHTML = "<hi>Today: " + date + "</h1>"
 alert("Hope it works!")
 
 console.log("today is" + date)
+
+
+var a;
+var b;
+var sum;
+a = 4;
+b = 5;
+sum = a + b;
+
+
+var string = "This should be a good string";
